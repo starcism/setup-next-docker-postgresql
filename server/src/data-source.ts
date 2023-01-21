d2eee2e97528ceb83d1685d66786ee66ff5d5b14
sangmoon <laugh120@naver.com>
2023-01-22 00:22:42 +0900

    setup-docker-postgresQL

---
+++ server/src/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "sangmoon",
+    password: "password",
+    database: "postgres",
+    synchronize: true, //개발환경에서 true, 운영환경에서 false
+    logging: false,
+    entities: [
+        "src/entities/**/*.ts"
+    ],
+    migrations: [],
+    subscribers: [],
+})
